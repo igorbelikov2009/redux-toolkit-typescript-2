f5d667472dafedad7dcdf28512c2347ff55dcf16
igor <igorbelikov2009@yandex.ru>
2022-11-13 17:50:22 +0300

    App usesrList

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,67 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import { useAppDispanch, useAppSelector } from "./hooks/redux";
+import { fetchUsers } from "./store/reducers/ActionCreater";
 import { userSlice } from "./store/reducers/UserSlice";
 
 function App() {
   const dispatch = useAppDispanch();
 
-  // console.log(increment(5)); // получаем самый обыкновенный редаксовский экшен
-  return <div className="App">App</div>;
+  // Вытаскиваем, необходимые для нас, данные из состояния
+  const { users, isLoading, error } = useAppSelector((state) => state.userReducer);
+  console.log(users, isLoading, error);
+
+  // Реализуем получение пользователей
+  useEffect(() => {
+    dispatch(fetchUsers);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div className="mt-2">
+      {users.map((user) => (
+        <div key={user.id} className="mb-1">
+          <i className="displayBlock">
+            <b> Пользователь под номером : </b> {user.id}
+          </i>
+
+          <i className="displayBlock">
+            <b> имя пользователя : </b> {user.name}
+          </i>
+
+          <i className="displayBlock">
+            <b>ник : </b> {user.username}
+          </i>
+          <i className="displayBlock">
+            <b>email :</b> {user.email}
+          </i>
+          <i className="displayBlock">
+            <b>телефон :</b> {user.phone}
+          </i>
+          <i className="displayBlock">
+            <b>ВЭБ сайт :</b> {user.website}
+          </i>
+
+          <i className="displayBlock">
+            <b> company name: </b> {user.company.name}
+          </i>
+          <i className="displayBlock">
+            <b>город :</b> {user.address.city}
+          </i>
+          <i className="displayBlock">
+            <b>улица :</b> {user.address.street}
+          </i>
+          <i className="displayBlock">
+            <b> suite :</b> {user.address.suite}
+          </i>
+          <i className="displayBlock">
+            <b>zipcode :</b>
+            {user.address.zipcode}
+          </i>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
--- src/models/IUser.ts
+++ src/models/IUser.ts
@@ -1,5 +1,19 @@
 export interface IUser {
   id: number;
   name: string;
+  username: string;
   email: string;
+  phone: string;
+  website: string;
+
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+  };
+
+  company: {
+    name: string;
+  };
 }
