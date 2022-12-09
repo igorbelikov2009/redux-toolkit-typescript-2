42fdf3708b44e542ff33effd6525b9ca706f1f98
igor <igorbelikov2009@yandex.ru>
2022-12-09 10:54:35 +0300

    post form

--- src/components/items/PostPaginationItem.tsx
+++ src/components/items/PostPaginationItem.tsx
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { FC } from "react";
 import Card from "react-bootstrap/Card";
 import { Button } from "react-bootstrap";
+import { IPost } from "../../models/types";
 
-const PostPaginationItem = () => {
-  return <div></div>;
+interface PostPaginationItemProps {
+  post: IPost;
+  remove: (post: IPost) => void;
+}
+
+const PostPaginationItem: FC<PostPaginationItemProps> = ({ post, remove }) => {
+  const handleRemove = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    remove(post);
+  };
+
+  return (
+    <Card className="post">
+      <div className="cardBlock">
+        <div className="cardDescription">
+          <i className="displayBlock">
+            Пост № <b> {post.id} </b>
+          </i>
+
+          <i className="displayBlock">
+            <b> Заглавие: </b> {post.title}.
+          </i>
+
+          <i className="displayBlock">
+            <b> Содержимое: </b> {post.body}
+          </i>
+        </div>
+
+        <div className="cardButton">
+          <Button variant="outline-danger" className="mt-2" onClick={handleRemove}>
+            Удалить
+          </Button>
+        </div>
+      </div>
+    </Card>
+  );
 };
 
 export default PostPaginationItem;
--- src/components/modal/PostForm.tsx
+++ src/components/modal/PostForm.tsx
@@ -1,12 +1,64 @@
-import React, { FC } from "react";
+import {
+  BaseQueryFn,
+  FetchArgs,
+  FetchBaseQueryError,
+  FetchBaseQueryMeta,
+  MutationDefinition,
+} from "@reduxjs/toolkit/dist/query/react";
+import { MutationTrigger } from "@reduxjs/toolkit/dist/query/react/buildHooks";
+import React, { FC, useState } from "react";
+
+import { Form, Button } from "react-bootstrap";
 import { IPost } from "../../models/types";
 
 interface PostFormProps {
-  create: (newPost: IPost) => void;
+  addPost: MutationTrigger<
+    MutationDefinition<
+      IPost,
+      BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError, {}, FetchBaseQueryMeta>,
+      "Post",
+      IPost,
+      "postPaginationAPI"
+    >
+  >;
 }
 
-const PostForm: FC<PostFormProps> = ({ create }) => {
-  return <div>PostForm</div>;
+const PostForm: FC<PostFormProps> = ({ addPost }) => {
+  const [newTitle, setNewTitle] = useState<string>("");
+  const [newBody, setNewBody] = useState<string>("");
+
+  const handleAddPost = async () => {
+    if (newTitle && newBody) {
+      await addPost({
+        title: newTitle,
+        body: newBody,
+        id: 0,
+      }).unwrap();
+      setNewTitle("");
+      setNewBody("");
+    }
+  };
+  return (
+    <div className="card mr-2">
+      <Form.Control
+        className="mb-2"
+        type="text"
+        value={newTitle}
+        onChange={(e) => setNewTitle(e.target.value)}
+        placeholder="Введите название нового поста"
+      />
+      <Form.Control
+        className="mb-2"
+        type="text"
+        value={newBody}
+        onChange={(e) => setNewBody(e.target.value)}
+        placeholder="Введите текст нового поста"
+      />
+      <Button variant="outline-success" onClick={handleAddPost}>
+        Добавить новый пост
+      </Button>
+    </div>
+  );
 };
 
 export default PostForm;
